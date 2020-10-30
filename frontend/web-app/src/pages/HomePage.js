4e3802fd13626c15531e856fb99fda6559c40b4d
DJ Poland <djpoland@knights.ucf.edu>
2020-10-30 10:54:20 -0400

    Implement left-text-box for login page

--- frontend/web-app/src/pages/HomePage.js
+++ frontend/web-app/src/pages/HomePage.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
+import LeftTextBox from "../components/LeftTextBox";
+
+import "../css/HomePage.css";
 
 const HomePage = () => {
   return (
     <div>
       <Header default />
+      <div className="middle-line" />
+      <LeftTextBox />
     </div>
   );
 };
