44bd77125922c05ea53220a7a55dd5ce44021e10
DJ Poland <djpoland@knights.ucf.edu>
2020-10-30 14:49:57 -0400

    Setup server hosting

--- frontend/web-app/server.js
+++ frontend/web-app/server.js
@@ -1,1 +1,17 @@
-// TODO: Implement backend server endpoints here.
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// 5000 is open on EC2
+const port = 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// Serve static assets if production (aka EC2)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
