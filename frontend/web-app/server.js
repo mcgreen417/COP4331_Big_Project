b7d3af42aaacdf3e0537c395558e93c931754fbd
DJ Poland <djpoland@knights.ucf.edu>
2020-11-02 18:56:25 -0500

    Fix credential issue with AWS

--- frontend/web-app/server.js
+++ frontend/web-app/server.js
@@ -1,64 +1,79 @@
 const express = require("express");
 const path = require("path");
-const bodyParser = require('body-parser');
-var AWS = require("aws-sdk");
-let awsConfig = {
-    "region": "us-east-2",
-    "endpoint": "http://dynamodb.us-east-2.amazonaws.com",
-    // Access Key of wizard
-    "accessKeyId": "AKIA53B2MINS4VX2KSEL", "secretAccessKey": "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"
-};
-AWS.config.update(awsConfig);
+const bodyParser = require("body-parser");
+const AWS = require("aws-sdk");
+AWS.config.getCredentials(function (err) {
+  if (err) {
+    console.log("AWS Credentials not configured properly");
+  } else {
+    console.log(
+      "Signed in with Access Key:",
+      AWS.config.credentials.accessKeyId
+    );
+  }
+});
 
 const app = express();
-
-// 5000 is open on EC2
 const port = 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let DynamoDB = new AWS.DynamoDB({
+  region: "us-east-2",
+  endpoint: "http://dynamodb.us-east-2.amazonaws.com",
+});
 
 // LOGIN API
-app.post('/api/login', (req, res) => {
-    
-    let documentClient = new AWS.DynamoDB.DocumentClient();
+app.post("/api/login", (req, res) => {
+  let documentClient = DynamoDB.DocumentClient();
 
-    let searchUser = function () {
-        var params = {
-          TableName : "Users",
-          FilterExpression : "Username = :this_user AND Password = :this_pass",
-          ExpressionAttributeValues : {
-              ":this_user" : req.body.username,
-              ":this_pass" : req.body.password
-          }
+  let searchUser = function () {
+    var params = {
+      TableName: "Users",
+      FilterExpression: "Username = :this_user AND Password = :this_pass",
+      ExpressionAttributeValues: {
+        ":this_user": req.body.username,
+        ":this_pass": req.body.password,
+      },
+    };
+
+    documentClient.scan(params, function (err, data) {
+      var uid = -1;
+      var mail = "";
+      var user = "";
+      var pass = "";
+      var error = "";
+
+      if (err) {
+        console.log(err);
+        var ret = {
+          UserID: uid,
+          Email: mail,
+          Username: user,
+          Password: pass,
+          Error: "Login Failed",
         };
-        
-        documentClient.scan(params, function(err, data) {
-
-          var uid = -1;
-          var mail = "";
-          var user = "";
-          var pass = "";
-          var error = "";
-
-          if (err) {
-              console.log(err);
-              var ret = { UserID:uid, Email:mail, Username:user, Password:pass, Error:"Login Failed" };
-              res.status(200).json(ret);
-          }
-          else {
-              data.Items.forEach(function(item) {
-                uid = item.UserID;
-                mail = item.Email;
-                user = item.Username;
-                pass = item.Password;
-                var ret = { UserID:uid, Email:mail, Username:user, Password:pass, Error:error };
-                res.status(200).json(ret);
-              });
-          }
+        res.status(200).json(ret);
+      } else {
+        data.Items.forEach(function (item) {
+          uid = item.UserID;
+          mail = item.Email;
+          user = item.Username;
+          pass = item.Password;
+          var ret = {
+            UserID: uid,
+            Email: mail,
+            Username: user,
+            Password: pass,
+            Error: error,
+          };
+          res.status(200).json(ret);
         });
-    }
-    searchUser();
+      }
+    });
+  };
+  searchUser();
 });
 
 // Serve static assets if production (aka EC2)
