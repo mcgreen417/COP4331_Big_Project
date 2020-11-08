82df3b5c15b3ad846346a2654ce3abf5e22be9c2
Aden Carver <99aden@gmail.com>
2020-11-07 19:23:57 -0500

    added editEntry API

--- frontend/web-app/server.js
+++ frontend/web-app/server.js
@@ -27,6 +27,53 @@ const app = new App({
   middleWares: [bodyParser.json(), bodyParser.urlencoded({ extended: true })],
 });
 
+//EDIT ENTRY API
+app.post('/api/editEntry', async (req, res, next) => {
+  //INCOMING: UserID, Nickname, Species, Classifications, Sunlight, Water, Reminders, Notes, Date Acquired
+  //OUTGOING: ID, Edits, Error
+
+  let updateItem = function() {
+    let documentClient = new AWS.DynamoDB.DocumentClient;
+
+    const { plantid, uid, nickname, species, sunlight, water, notes, date, classifications, reminders } = req.body;
+
+    const params = {
+      TableName: Plants,
+      Key: {
+        "UserID": uid,
+        "PlantID": plantid
+      },
+      UpdateExpression: "set Nickname = :thisNick, Species = :thisSpecies, Sunlight = :thisSunlight, Water = :thisWater, Notes = :thisnotes, Date Acquired = :thisDate, Classifications = :thisClass, Reminders = :thisReminders",
+      ExpressionAttributes: {
+        ":thisNick": nickname,
+        ":thisSpecies": species,
+        ":thisClass": classifications,
+        ":thisSunlight": sunlight,
+        ":thisWater": water,
+        ":thisReminders": reminders,
+        ":thisNotes": notes,
+        ":thisDate": date
+      },
+      ReturnValues:"UPDATED_NEW"
+    };
+
+    //update table
+    documentClient.update(params, function(err, data) {
+      if(err) {
+        console.log(" Failed To Update Item ");
+        var retVal = { PlantID:plantid, UserID: uid, Nickname: nickname, Error:err };
+        res.status(200).json(retVal);
+      } else {
+        console.log(" Successfully Updated Item ");
+        var retVal = { PlantID:plantid, UserID:uid, Nickname:nickname, Error:'' };
+        res.status(200).json(retVal);
+      }
+    });
+  }
+
+  updateItem();
+});
+
 // Serve static assets if production (aka EC2)
 if (process.env.NODE_ENV === "production") {
   console.log("Running server in PRODUCTION mode");
