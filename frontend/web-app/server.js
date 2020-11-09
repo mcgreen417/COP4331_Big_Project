795a6a6df7d2742d2bbcff65f01c36f7210a5123
Aden Carver <99aden@gmail.com>
2020-11-09 17:41:31 -0500

    moved edit entry

--- frontend/web-app/server.js
+++ frontend/web-app/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const AWS = require("aws-sdk");
 const App = require("./app");
 const AuthController = require("./controllers/auth.controller");
@@ -25,53 +26,6 @@ const app = new App({
   port: 5000,
   controllers: [new AuthController(), new ProtectedController()],
   middleWares: [bodyParser.json(), bodyParser.urlencoded({ extended: true })],
-});
-
-//EDIT ENTRY API
-app.post('/api/editEntry', async (req, res, next) => {
-  //INCOMING: UserID, Nickname, Species, Classifications, Sunlight, Water, Reminders, Notes, Date Acquired
-  //OUTGOING: ID, Edits, Error
-
-  let updateItem = function() {
-    let documentClient = new AWS.DynamoDB.DocumentClient;
-
-    const { plantid, uid, nickname, species, sunlight, water, notes, date, classifications, reminders } = req.body;
-
-    const params = {
-      TableName: Plants,
-      Key: {
-        "UserID": uid,
-        "PlantID": plantid
-      },
-      UpdateExpression: "set Nickname = :thisNick, Species = :thisSpecies, Sunlight = :thisSunlight, Water = :thisWater, Notes = :thisnotes, Date Acquired = :thisDate, Classifications = :thisClass, Reminders = :thisReminders",
-      ExpressionAttributes: {
-        ":thisNick": nickname,
-        ":thisSpecies": species,
-        ":thisClass": classifications,
-        ":thisSunlight": sunlight,
-        ":thisWater": water,
-        ":thisReminders": reminders,
-        ":thisNotes": notes,
-        ":thisDate": date
-      },
-      ReturnValues:"UPDATED_NEW"
-    };
-
-    //update table
-    documentClient.update(params, function(err, data) {
-      if(err) {
-        console.log(" Failed To Update Item ");
-        var retVal = { PlantID:plantid, UserID: uid, Nickname: nickname, Error:err };
-        res.status(200).json(retVal);
-      } else {
-        console.log(" Successfully Updated Item ");
-        var retVal = { PlantID:plantid, UserID:uid, Nickname:nickname, Error:'' };
-        res.status(200).json(retVal);
-      }
-    });
-  }
-
-  updateItem();
 });
 
 // Serve static assets if production (aka EC2)
